a81209185dfc914805a2679f0db30c9637ef3cb5
Colin Reeder <vpzomtrrfrt@gmail.com>
2015-12-06 21:14:37 +0000

    Add booleans

--- emoji.py
+++ emoji.py
@@ -2,15 +2,19 @@
 import sys
 def parseChar(data, char):
 	if data["string"] != None:
-		if char == "\U0001f4ac": # speech balloon
+		if char == "\U0001f4ac": # speech balloon (end string)
 			data["stack"].append(data["string"])
 			data["string"] = None
 		else:
 			data["string"] += char
-	if char == "\U0001f4ac": # speech balloon
+	if char == "\U0001f4ac": # speech balloon (begin string)
 		data["string"] = ""
-	elif char == "\u27a1": # black rightwards arrow
+	elif char == "\u27a1": # black rightwards arrow (print)
 		print(data["stack"].pop())
+	elif char == "\U0001f6b2": # bicycle (true)
+		data["stack"].append(True)
+	elif char == "\U0001f6b3": # no bicycles (false)
+		data["stack"].append(False)
 def emojiEval(s, stack=[]):
 	data = {
 		"string": None,
